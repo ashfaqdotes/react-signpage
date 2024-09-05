9182e535886d495415323348604406142a2857a1
Ashfaq Ahmed <ashfaqdotes@gmail.com>
2024-09-05 12:25:30 +0500

    change

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,7 +2,7 @@ import SignUp from "./sign-up";
 import SignIn from "./sign-in";
 export default function Card() {
   return (
-    <div className="sm:1/1 lg:w-1/2 mt-20 bg-slate-300 mx-auto">
+    <div className="sm:1/1 lg:w-1/2 mt-20 mx-auto">
       <SignUp />
     </div>
   );
